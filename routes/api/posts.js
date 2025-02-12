59fd69c7a00a969b6b5b02e1b436135c52a94780
Aman Brar <aman1531@gmail.com>
2025-02-12 15:56:47 +0530

    tags api

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,28 @@
 import { processMarkdownPosts } from "../../functions/helpers/processMarkdownPosts.js"
+import { countPostsByTag, getPostsByTag } from "../../functions/helpers/processPostsTags.js"
+
 
 export const apiRoutes = (app) => {
     app.get('/api/posts', async (req , res) => {
         const posts = await processMarkdownPosts(app);
         res.writeHead(200, { "Content-Type": "application/json" })
         res.end(JSON.stringify(posts));
-    });
-    
-} 
+    })
+   // tag routes
+	    .get("/api/tags", async (req, res) => {
+		    const tagsCount = await countPostsByTag(app)
+		      res.writeHead(200, {"Content-Type": "application/json"})
+          res.end(JSON.stringify(tagsCount));
+	      
+      })
+
+		   .get("/api/tags/:tag", async (req, res) => {
+			      const tag = req.params.tag
+			      // List of posts matching the `:tag` request parameter
+			      const postsByTag = await getPostsByTag(app, tag)
+           res.writeHead(200, {"Content-Type": "application/json"})
+           res.end(JSON.stringify(postsByTag));
+	         
+       })
+
+}
